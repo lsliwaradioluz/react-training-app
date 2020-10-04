a6c5528e576c95bc57ce205879fbf0328ce5b54c
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-10-04 10:01:26 +0200

    handle cookies user undefined case in hydrate store in App.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,6 @@ class App extends Component {
   }
 
   hydrateStore() {
-    console.log('dupa')
     let user = Cookies.get("user");
     if (user !== undefined) {
       this.props.setUser(user && JSON.parse(user));
